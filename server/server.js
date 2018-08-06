6ca58112075f7533dcc86e7d1cdf5b734e284dc0
Jurek <jurek2006@gmail.com>
2018-08-06 14:58:58 +0200

    Podstawowe eventy connect i disconnect

--- server/server.js
+++ server/server.js
@@ -1,15 +1,27 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
+io.on('connection', socket => {
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+});
+
 if(!module.parent){
-    app.listen(port, () => {
+    server.listen(port, () => {
         console.log(`Serwer uruchomiony na porcie ${port}`);
     });
 }
