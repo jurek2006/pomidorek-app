9fec04a55e9b59253894c8e7c376a1152d68b0b5
Jurek <jurek2006@gmail.com>
2018-09-03 14:12:23 +0200

    Test podstawowej obsługi przycisków czasu

--- public/js/index.js
+++ public/js/index.js
@@ -5,4 +5,47 @@ socket.on('connect', () => {
 });
 socket.on('disconnect', () => {
     console.log('Disconnected from server');
-})
+});
+
+
+
+const timerElement = document.querySelector('#timer');
+const timerTime = timerElement.querySelector('.time');
+const timerStartBtns = timerElement.querySelectorAll('.timer__start');
+const timerPauseBtns = timerElement.querySelectorAll('.timer__pause');
+const timerStopBtns = timerElement.querySelectorAll('.timer__stop');
+
+const timer = {
+    startTime: false,
+    timeout: false,
+    start: () => {
+        timer.time = new Date();
+        timerTime.textContent = timer.time;
+        timer.timeout = setTimeout(timer.start, 500);
+    },
+    stop: () => {
+        clearTimeout(timer.timeout);
+        timerTime.textContent = '00:00';
+    },
+    pause: () => {
+        clearTimeout(timer.timeout);
+        timerTime.textContent += ' PAUSED';
+    }
+}
+
+// przypisanie obsługi zdarzeń do przycisków
+timerStartBtns.forEach(button => button.addEventListener('click', () => {
+    timer.start();
+    console.log('start');
+}));
+
+timerPauseBtns.forEach(button => button.addEventListener('click', () => {
+    timer.pause();
+    console.log('pause');
+}));
+
+timerStopBtns.forEach(button => button.addEventListener('click', () => {
+    timer.stop();
+    console.log('stop');
+}));
+
